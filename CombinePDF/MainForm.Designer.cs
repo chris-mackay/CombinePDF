de2429d796e36c0f1cb32ebcd2574b92a1c2f312
Christopher Mackay <cmackay732@gmail.com>
2021-03-10 19:40:38 -0500

    Added code to Browse button

--- CombinePDF/MainForm.Designer.cs
+++ CombinePDF/MainForm.Designer.cs
@@ -34,20 +34,23 @@ namespace CombinePDF
             this.btnMoveUp = new System.Windows.Forms.Button();
             this.btnMoveDown = new System.Windows.Forms.Button();
             this.lblFileDirectory = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
             this.btnBrowse = new System.Windows.Forms.Button();
             this.lstFiles = new System.Windows.Forms.ListBox();
             this.btnAddFile = new System.Windows.Forms.Button();
             this.btnRemoveFile = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClose.Location = new System.Drawing.Point(447, 476);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
-            this.btnClose.TabIndex = 0;
+            this.btnClose.TabIndex = 11;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             // 
@@ -57,7 +60,7 @@ namespace CombinePDF
             this.btnCombine.Location = new System.Drawing.Point(366, 476);
             this.btnCombine.Name = "btnCombine";
             this.btnCombine.Size = new System.Drawing.Size(75, 23);
-            this.btnCombine.TabIndex = 1;
+            this.btnCombine.TabIndex = 10;
             this.btnCombine.Text = "Combine";
             this.btnCombine.UseVisualStyleBackColor = true;
             // 
@@ -67,7 +70,7 @@ namespace CombinePDF
             this.btnMoveUp.Location = new System.Drawing.Point(12, 476);
             this.btnMoveUp.Name = "btnMoveUp";
             this.btnMoveUp.Size = new System.Drawing.Size(75, 23);
-            this.btnMoveUp.TabIndex = 2;
+            this.btnMoveUp.TabIndex = 8;
             this.btnMoveUp.Text = "Move Up";
             this.btnMoveUp.UseVisualStyleBackColor = true;
             // 
@@ -77,37 +80,38 @@ namespace CombinePDF
             this.btnMoveDown.Location = new System.Drawing.Point(93, 476);
             this.btnMoveDown.Name = "btnMoveDown";
             this.btnMoveDown.Size = new System.Drawing.Size(75, 23);
-            this.btnMoveDown.TabIndex = 3;
+            this.btnMoveDown.TabIndex = 9;
             this.btnMoveDown.Text = "Move Down";
             this.btnMoveDown.UseVisualStyleBackColor = true;
             // 
             // lblFileDirectory
             // 
             this.lblFileDirectory.AutoSize = true;
-            this.lblFileDirectory.Location = new System.Drawing.Point(9, 36);
+            this.lblFileDirectory.Location = new System.Drawing.Point(9, 42);
             this.lblFileDirectory.Name = "lblFileDirectory";
             this.lblFileDirectory.Size = new System.Drawing.Size(68, 13);
-            this.lblFileDirectory.TabIndex = 4;
+            this.lblFileDirectory.TabIndex = 0;
             this.lblFileDirectory.Text = "File Directory";
             // 
-            // textBox1
+            // txtDirectory
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(83, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(358, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtDirectory.Location = new System.Drawing.Point(83, 34);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(358, 20);
+            this.txtDirectory.TabIndex = 1;
             // 
             // btnBrowse
             // 
             this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowse.Location = new System.Drawing.Point(447, 26);
+            this.btnBrowse.Location = new System.Drawing.Point(447, 32);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.TabIndex = 2;
             this.btnBrowse.Text = "Browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // lstFiles
             // 
@@ -115,39 +119,62 @@ namespace CombinePDF
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstFiles.FormattingEnabled = true;
-            this.lstFiles.Location = new System.Drawing.Point(12, 107);
+            this.lstFiles.Location = new System.Drawing.Point(12, 159);
             this.lstFiles.Name = "lstFiles";
-            this.lstFiles.Size = new System.Drawing.Size(510, 355);
+            this.lstFiles.Size = new System.Drawing.Size(510, 303);
             this.lstFiles.TabIndex = 7;
             // 
             // btnAddFile
             // 
-            this.btnAddFile.Location = new System.Drawing.Point(12, 78);
+            this.btnAddFile.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAddFile.Location = new System.Drawing.Point(12, 130);
             this.btnAddFile.Name = "btnAddFile";
             this.btnAddFile.Size = new System.Drawing.Size(75, 23);
-            this.btnAddFile.TabIndex = 8;
+            this.btnAddFile.TabIndex = 5;
             this.btnAddFile.Text = "Add File";
             this.btnAddFile.UseVisualStyleBackColor = true;
             // 
             // btnRemoveFile
             // 
-            this.btnRemoveFile.Location = new System.Drawing.Point(93, 78);
+            this.btnRemoveFile.Location = new System.Drawing.Point(93, 130);
             this.btnRemoveFile.Name = "btnRemoveFile";
             this.btnRemoveFile.Size = new System.Drawing.Size(75, 23);
-            this.btnRemoveFile.TabIndex = 9;
+            this.btnRemoveFile.TabIndex = 6;
             this.btnRemoveFile.Text = "Remove File";
             this.btnRemoveFile.UseVisualStyleBackColor = true;
             // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(80, 64);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(296, 26);
+            this.lblDescription.TabIndex = 3;
+            this.lblDescription.Text = "Specifying a directory will load all PDF files from the directory. \r\nNot includin" +
+    "g sub-directories.";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(17, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 1);
+            this.label1.TabIndex = 4;
+            // 
             // MainForm
             // 
+            this.AcceptButton = this.btnCombine;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
             this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.btnRemoveFile);
             this.Controls.Add(this.btnAddFile);
             this.Controls.Add(this.lstFiles);
             this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtDirectory);
             this.Controls.Add(this.lblFileDirectory);
             this.Controls.Add(this.btnMoveDown);
             this.Controls.Add(this.btnMoveUp);
@@ -160,6 +187,7 @@ namespace CombinePDF
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Combine PDF";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,11 +200,13 @@ namespace CombinePDF
         private System.Windows.Forms.Button btnMoveUp;
         private System.Windows.Forms.Button btnMoveDown;
         private System.Windows.Forms.Label lblFileDirectory;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDirectory;
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.ListBox lstFiles;
         private System.Windows.Forms.Button btnAddFile;
         private System.Windows.Forms.Button btnRemoveFile;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ CombinePDF/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace CombinePDF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtDirectory.Text = dialog.FileName;
+            }
+        }
+    }
+}
